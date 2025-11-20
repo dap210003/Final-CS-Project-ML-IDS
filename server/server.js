839520a4fd4e8fda787d34093be3a820e190e611
dap210003 <114041014+dap210003@users.noreply.github.com>
2025-11-20 16:42:14 -0600

    Finished writing out insert statements for all tables prior to running actual model and getting performance metrics from it. Will work to do calls to python for the later section

--- server/server.js
+++ server/server.js
@@ -40,10 +40,187 @@ app.use(function(req, res, next) {
 });
 //TODO: place queries for add a specific row to db, search out a specific row for db, delete a row for db
 
-async function AddDb(/*information for each table should go here */id){//this should add the given item to the particular node
+async function AddDb(/*information for each table should go here */id,model_select,dataset_selection,user_id,parameter){//this should add the given item to the particular node
+//make string for query, switch-statement on type of database requested
+database_query= new String();
+database_values=new Array(3);
+model_choice=model_select;//variable here for tracking what type of model is being used, same as model_select value.
+switch(model_select){
+case 1://LCCDE?
+database_query="INSERT INTO ml_models (model_name, model_type,description,) VALUES (?,?,?) RETURNING model_id";
+database_values[0]="Light-GBM";
+database_values[1]="gradient_boosting";
+database_values[2]="";
+break;
+
+case 2://other data types
+database_query="INSERT INTO ml_models (model_name, model_type,description,) VALUES (?,?,?) RETURNING model_id";
+database_values[0]="XGBOOST";
+database_values[1]="gradient_boosting";
+database_values[2]="";
+break;
+
+case 3:
+database_query="INSERT INTO ml_models (model_name, model_type,description,) VALUES (?,?,?) RETURNING model_id";
+database_values[0]="CatBoost";
+database_values[1]="gradient_boosting";
+database_values[2]="";
+break;
+case 4:
+database_query="INSERT INTO ml_models (model_name, model_type,description,) VALUES (?,?,?) RETURNING model_id";
+database_values[0]="LCCDE";
+database_values[1]="ensemble";
+database_values[2]="";
+break;
+
+
+default:
+database_query="INSERT INTO ml_models (model_name, model_type,description,) VALUES (?,?,?) RETURNING model_id";
+database_values[0]="LCCDE";
+database_values[1]=model_type;
+database_values[2]="";
+break;
+}
+current_model_id=pool.query(database_query,database_values);
+
+//grab dataset id from here before???
+dataset_query=new String();
+dataset_info=new Array(9);
+switch(dataset_selection){
+case 1:
+
+break;
+
+case 2:
+
+break;
+
+default:
+
+break;
+
+}
+
+
+dataset_id=dataset_selection;//for now do it like this? ,maybe run some other method for adding dataset to it?
+dataset_value=new String();
+switch(dataset_id){//this decides what datsets should be used for the description
+case 1:
+dataset_value="Dataset 1";
+break;
+
+case 2:
+dataset_value="Dataset 2";
+break;
+
+default:
+dataset_value="";
+break;
+
+}
+
+//from here do query to add to training runs: find some user id, grab the model_id from a prior, 
+user_id=id;//???
+training_run_id=pool.query("INSERT INTO training_runs(user_id,model_id,dataset_id,run_name) VALUES(?,?,?,?) RETURNING run_id",[user_id,current_model_id,dataset_id,"Training_run for user"+user_id+" "+dataset_value]);
+//need to find some place to grab parameter ids prior to this.one each for them, LCCDE grab all in totality
+parameterHolder=new Array();
+if(model_select!=4/*NOT a LCCDE */){
+[parameterHolder]=pool.query("SELECT parameter_id FROM model_parameters WHERE model_id=?",[model_select]);
+}
+else{//attach the 3 for LCCDE
+[LGBMHolder]=await pool.query("SELECT parameter_id FROM model_parameters WHERE model_id=?",[1]);
+[XGBHolder]=await pool.query("SELECT parameter_id FROM model_parameters WHERE model_id=?",[2]);
+[CATHolder]=await pool.query("SELECT parameter_id FROM model_parameters WHERE model_id=?",[3]);
+parameterHolder.push(...LGBMHolder);
+parameterHolder.push(...XGBHolder);
+parameterHolder.push(...CATHolder);
+}
+//this should place parameters in list so I can later check them
+
+//from here go for adding in run parameters, this will have to be a series of  parameters input by the user prior, maybe store them in an array beforehand?
+//need some kind of array to be able to store the items for the condition
+//maybe array length to store all values guaranteed, case by case only filling in a few of them??
+//we'd use model select as the passer variable for this as well
+//array would be  12+14+15=41 spots long???
+switch(model_select){
+case 1://LightGBM
+//adds each parameter to the function
+await pool.query("Insert INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[0],parameter[0]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[1],parameter[1]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[2],parameter[2]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[3],parameter[3]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[4],parameter[4]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[5],parameter[5]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[6],parameter[6]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[7],parameter[7]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[8],parameter[8]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[9],parameter[9]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[10],parameter[10]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[11],parameter[11]]);
 
 
 
+break;
+
+case 2://XGBOOST
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[0],parameter[0]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[1],parameter[1]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[2],parameter[2]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[3],parameter[3]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[4],parameter[4]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[5],parameter[5]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[6],parameter[6]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[7],parameter[7]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[8],parameter[8]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[9],parameter[9]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[10],parameter[10]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[11],parameter[11]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[12],parameter[12]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[13],parameter[13]]);
+
+break;
+
+case 3://CatBoost
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[0],parameter[0]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[1],parameter[1]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[2],parameter[2]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[3],parameter[3]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[4],parameter[4]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[5],parameter[5]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[6],parameter[6]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[7],parameter[7]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[8],parameter[8]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[9],parameter[9]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[10],parameter[10]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[11],parameter[11]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[12],parameter[12]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[13],parameter[13]]);
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[14],parameter[14]]);
+
+break;
+
+
+case 4://LCCDE
+//add all of the previoud queries up
+//some kind of for loop for the idea?
+for(let i=0;i<parameterHolder.length;i++){
+await pool.query("INSERT INTO run_parameters(run_id,parameter_id,parameter_value) VALUES (?,?,?)",[training_run_id,parameterHolder[i],parameter[i]]);
+
+}
+break;
+
+default://put something here for this adder?
+
+break;
+
+}
+//preprocessing configuration goes here
+configTracker=await pool.query("INSERT INTO preprocessing_config(run_id,train_test_split_ratio,random_state,use_smote,smote_strategy,feature_scaling_method) VALUES (?,?,?,?,?,?) RETURNING config_id",[training_run_id,train_test_ratio,random_state,use_smote,smote_strategy,feature_scaling]);
+
+
+//here calls to ML methods for python integration(maybe flask???)
+
+  return true;//placeholder return value
 }
 
 async function grabAllDB(){
@@ -53,7 +230,19 @@ async function grabAllDB(){
 
 async function searchDb(id){
 //grab from all necessary tables for the given item(parameters, dataset, evaluation metrics,etc.)?
-let rows=new Array(9);
+
+
+
+
+let rows=new Array(5);
+rows[0]=await pool.query('SELECT * FROM training_runs WHERE run_id=?'[id]);
+rows[1]=await pool.query('SELECT * FROM run_parameters WHERE run_id=?',[id]);
+rows[2]=await pool.query('SELECT * FROM preprocessing_config WHERE run_id=?',[id]);
+rows[3]=await pool.query('SELECT * FROM run_results WHERE run_id=?',[id]);
+rows[4]=await pool.query('SELECT * FROM model_artifacts WHERE run_id=?',[id]);
+//maybe make query to grab other identifier to grab more info from tables?
+
+/*let rows=new Array(9);
 //I think this should cover everything we want to  fetch for a given environmental run?
 rows[0]=await pool.query('SELECT * FROM users WHERE user_id=?',[id]);
 rows[1]=await pool.query('SELECT * FROM ml_models WHERE model_id=?',[id]);
@@ -63,12 +252,12 @@ rows[4]=await pool.query('SELECT * FROM training_runs WHERE user_id=? ',[id]);
 rows[5]=await pool.quert('SELECT * FROM run_parameters WHERE parameter_id=?',[id]);
 rows[6]=await pool.query('SELECT * FROM preprocessing_config WHERE run_id=?',[id]);
 rows[7]=await pool.query('SELECT * FROM run_results WHERE run_id=?',[id]);
-rows[8]=await pool.query('SELECT * FROM class_performance WHERE result_id=?'[id]);
+rows[8]=await pool.query('SELECT * FROM class_performance WHERE result_id=?'[id]);*/
 return rows;
 }
 async function deleteDb(id){//deletes given row from the record(probably use this for when we need to modify a value in system)
   await pool.query('DELETE * FROM users WHERE user_id=?',[id]);
-  return;
+  return true;//boolean to check if deletion did actually occur.
 }
 
 
@@ -87,8 +276,10 @@ res.send(item);
 app.post('/',async (req,res)=>{//post response:should cover same idea as get, maybe could use this to cover other retrieval stuff?
 
 })
-app.put('/',async (req,res)=>{//place data into the database from the front end??
+app.put('/addItem',async (req,res)=>{//place data into the database from the front end??
 
+  const item=await AddDb();
+  res.send(item);
 })
 
 
